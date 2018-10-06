8203a506563eb10547462d15e92f47a38267b62e
CharafLachachi <lac.charaf@gmail.com>
2018-10-06 17:29:06 +0200

    Add CitiesOfInterest beans

--- DAR_PROJECT/src/main/java/com/beans/CitiesOfInterest.java
+++ DAR_PROJECT/src/main/java/com/beans/CitiesOfInterest.java
@@ -1,5 +1,26 @@
 package com.beans;
 
-public class CitiesOfInterest {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class CitiesOfInterest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "CITY_ID")
+	private int city_id;
+	
+	@Column(name ="CITY_NAME")
+	private String city_name;
+	
+	@Column(name ="CITY_LON")
+	private double city_longitude;
+	
+	@Column(name ="CITY_LAT")
+	private double city_latitude;
+	
+	
+	
+	
 }
